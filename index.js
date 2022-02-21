bd2af6ac4fd56c993abb93a973641be6ea1b10cb
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-02-21 22:59:00 +0000

    Refactor index.js file

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("hello world")
+import Book from './modules/Book.js';
+
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const add = document.getElementById('add');
+
+const book = new Book();
+
+add.addEventListener('click', (e) => {
+  e.preventDefault();
+  const Title = title.value;
+  const Author = author.value;
+
+  const newBook = {
+    title: Title,
+    author: Author,
+  };
+
+  if (Title === '' || Author === '') {
+    alert('please fill all fields');
+    return;
+  }
+
+  book.SaveBook(newBook);
+  alert('book saved successuflly');
+  title.value = '';
+  author.value = '';
+});
