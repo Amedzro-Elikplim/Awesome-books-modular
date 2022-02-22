d7f25a4303c7d71b0dcc1116b59ecfe3c1ba3946
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-02-22 16:50:44 +0000

    Change regular functions to arrow functions

--- modules/Date.js
+++ modules/Date.js
@@ -1,7 +1,9 @@
 import { DateTime } from '../node_modules/luxon/src/luxon.js';
 
-export default function showDate() {
+const showDate = () => {
   const now = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
   const div = document.querySelector('.date');
   div.innerHTML = now;
-}
+};
+
+export default showDate;
--- modules/Template.js
+++ modules/Template.js
@@ -1,17 +1,17 @@
-function li1(title) {
+const li1 = (title) => {
   const li = document.createElement('li');
   li.className = 'book-title';
   li.innerHTML = `"${title}" by`;
   return li;
-}
+};
 
-function li2(author) {
+const li2 = (author) => {
   const li = document.createElement('li');
   li.className = 'book-author';
   li.innerHTML = author;
 
   return li;
-}
+};
 
 export {
   li1,
