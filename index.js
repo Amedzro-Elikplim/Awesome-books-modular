e414a237c36259aa01ce8e70a7cc2552b589845d
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-02-22 15:11:37 +0000

    Import showDate function to display date on page load

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import Book from './modules/Book.js';
+import showDate from './modules/Date.js';
 
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const add = document.getElementById('add');
 
 const book = new Book();
+showDate();
 
 add.addEventListener('click', (e) => {
   e.preventDefault();
