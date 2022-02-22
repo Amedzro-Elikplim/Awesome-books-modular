58158e5c36c721c0464cc861912586a03d23a65b
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-02-22 00:09:34 +0000

    Refactor BookTemplate function into smaller module

---
+++ modules/Template.js
@@ -0,0 +1,39 @@
+import Book from './Book.js';
+
+function li1(title) {
+  const li = document.createElement('li');
+  li.className = 'book-title';
+  li.innerHTML = `"${title}" by`;
+  return li;
+}
+
+function li2(author) {
+  const li = document.createElement('li');
+  li.className = 'book-author';
+  li.innerHTML = author;
+
+  return li;
+}
+
+function button(title) {
+  const book = new Book();
+  const button = document.createElement('button');
+  button.className = 'remove-button';
+  button.innerHTML = 'remove';
+
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    book.RemoveBook(title);
+    console.log('hello')
+  });
+
+  return button;
+}
+
+
+export {
+  ul,
+  li1,
+  li2,
+  button,
+}
